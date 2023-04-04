6ba0c7b1eb2a44c7e6ba11f9bac77fa191445998
emptybrother7 <emptybrother7@gmail.com>
2023-04-04 12:55:19 -0500

    added env file

--- TFDB.py
+++ TFDB.py
@@ -3,10 +3,14 @@ import discord
 import json
 import requests
 from discord.ext import tasks
+from dotenv import load_dotenv
 
-# Your Twitch Dev API IDs
-twitch_client_id = '<Client ID>'
-twitch_client_secret = '<Client Secret ID>'
+load_dotenv()  # Load API credentials and bot token from .env file
+
+# Set up Twitch API
+twitch_client_id = os.environ.get('TWITCH_CLIENT_ID')
+twitch_client_secret = os.environ.get('TWITCH_CLIENT_SECRET')
+twitch_user_id = os.environ.get('TWITCH_USER_ID')
 
 def get_twitch_access_token():
     url = "https://id.twitch.tv/oauth2/token"
@@ -21,16 +25,13 @@ def get_twitch_access_token():
 
 twitch_access_token = get_twitch_access_token()
 
+# Set up Discord bot
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 client = discord.Client(intents=intents)
 
-# Your Twitch user ID
-twitch_user_id = '<Twitch User ID>'
-
-# Your Discord bot token
-discord_bot_token = '<Discord Bot Token>'
+discord_bot_token = os.environ.get('DISCORD_BOT_TOKEN')
 
 @client.event
 async def on_ready():
@@ -47,7 +48,7 @@ def get_follower_count():
     data = json.loads(response.text)
     return data["total"]
 
-@tasks.loop(minutes=20)
+@tasks.loop(minutes=360)
 async def update_status():
     followers = get_follower_count()
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{followers} followers"))
